9aad229ebe35dd09c8daeb85fd5b731e82681b53
Joe Runde <Joseph.Runde@ibm.com>
2023-06-20 17:10:06 -0600

    :white_check_mark: add sample http test case
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- caikit/runtime/http_server.py
+++ caikit/runtime/http_server.py
@@ -113,6 +113,11 @@ class RuntimeHTTPServer(RuntimeServerBase):
         # Bind all routes to the server
         self._bind_routes(inference_service)
 
+    def __del__(self):
+        if get_config().runtime.metering.enabled:
+            self.global_predict_servicer.rpc_meter.flush_metrics()
+            self.global_predict_servicer.rpc_meter.end_writer_thread()
+
     def start(self):
         """Start the server (blocking)"""
         # Parse TLS configuration
